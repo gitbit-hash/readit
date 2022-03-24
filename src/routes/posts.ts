a3ac42cc0f089a186e7eda3662679819d0d12a6f
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-25 00:55:43 +0200

    adding routes for get all posts and get a post by identifier and slug

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -35,8 +35,41 @@ const createPost = async (req: Request, res: Response) => {
 	}
 };
 
+const getAllPosts = async (_: Request, res: Response) => {
+	try {
+		const posts = await AppDataSource.getRepository(Post).find({
+			order: { createdAt: 'DESC' },
+		});
+
+		return res.json(posts);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: 'Some thing went wrong!' });
+	}
+};
+
+const getPost = async (req: Request, res: Response) => {
+	const { identifier, slug } = req.params;
+	try {
+		const post = await AppDataSource.getRepository(Post).findOneOrFail({
+			where: {
+				identifier,
+				slug,
+			},
+			relations: ['sub'],
+		});
+
+		return res.json(post);
+	} catch (error) {
+		console.log(error);
+		return res.status(404).json({ error: 'Post not found' });
+	}
+};
+
 const router = Router();
 
 router.post('/', auth, createPost);
+router.get('/', getAllPosts);
+router.get('/:identifier/:slug', getPost);
 
 export default router;
