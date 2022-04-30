1d3a9134b8f2dbc1efd08c6b9d4c0d3c709a886e
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-04-30 03:41:50 +0300

    feat: implementing image upload using multer with validation

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,5 +1,7 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { isEmpty } from 'class-validator';
+import multer, { FileFilterCallback } from 'multer';
+import path from 'path';
 
 import { User } from '../entities/User';
 import { Sub } from '../entities/Sub';
@@ -9,6 +11,9 @@ import { AppDataSource } from '../data-source';
 
 import { auth } from '../middlewares/auth';
 import { user } from '../middlewares/user';
+
+import { makeId } from '../utils/helpers';
+import fs from 'fs';
 
 const createSub = async (req: Request, res: Response) => {
 	const { name, title, description } = req.body;
@@ -73,9 +78,90 @@ const getSub = async (req: Request, res: Response) => {
 	}
 };
 
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+	const user: User = res.locals.user;
+
+	try {
+		const sub = await AppDataSource.getRepository(Sub).findOneByOrFail({
+			name: req.params.name,
+		});
+
+		if (sub.username !== user.username) {
+			return res.status(403).json({ error: 'You dont own this sub' });
+		}
+
+		res.locals.sub = sub;
+
+		return next();
+	} catch (error) {
+		return res.status(500).json({ error: 'Somethong went wrong!' });
+	}
+};
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination: 'puplic/images',
+		filename: (_, file, callback) => {
+			const name = makeId(15);
+			callback(null, name + path.extname(file.originalname)); // e.g. af342d + (png | jpg)
+		},
+	}),
+	fileFilter: (_, file: any, callback: FileFilterCallback) => {
+		console.log(file);
+		if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+			console.log('file type');
+			callback(null, true);
+		} else {
+			console.log('error');
+			callback(new Error('Not an Image file'));
+		}
+	},
+});
+
+const uploadSubImage = async (req: Request, res: Response) => {
+	const sub: Sub = res.locals.sub;
+
+	try {
+		const type = req.body.type;
+
+		if (type !== 'image' && type !== 'banner') {
+			fs.unlinkSync(req.file.path);
+			return res.status(500).json({ error: 'Invalid type' });
+		}
+
+		let oldImageUrn: string = '';
+
+		if (type === 'image') {
+			oldImageUrn = sub.imageUrn || '';
+			sub.imageUrn = req.file.filename;
+		} else if (type === 'banner') {
+			oldImageUrn = sub.bannerUrn || '';
+			sub.bannerUrn = req.file.filename;
+		}
+
+		await sub.save();
+
+		if (oldImageUrn !== '') {
+			fs.unlinkSync(`puplic\\images\\${oldImageUrn}`);
+		}
+
+		return res.json(sub);
+	} catch (error) {
+		return res.status(500).json({ error: 'Somethong went wrong!' });
+	}
+};
+
 const router = Router();
 
 router.post('/', user, auth, createSub);
 router.get('/:name', user, getSub);
+router.post(
+	'/:name/image',
+	user,
+	auth,
+	ownSub,
+	upload.single('file'),
+	uploadSubImage
+);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,7 @@ app.use(
 		optionsSuccessStatus: 200,
 	})
 );
+app.use(express.static('puplic'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
